70754b84b87306c0bbc20e06c08b6c8f379d8700
sneachda <savela.aleks@gmail.com>
2020-05-09 16:10:37 +0100

    updated all files

--- app.py
+++ app.py
@@ -4,9 +4,7 @@ from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
 
 app = Flask(__name__)
-app.config["MONGO_DBNAME"] = 'task_manager'
 app.config["MONGO_URI"] = "mongodb://localhost:27017/task_manager"
-
 mongo = PyMongo(app)
 
 
@@ -15,8 +13,22 @@ mongo = PyMongo(app)
 def get_tasks():
     return render_template("tasks.html", tasks=mongo.db.tasks.find())
 
+@app.route('/add_task')
+def add_task():
+    return render_template("addtask.html", categories=mongo.db.categories.find())
+
+@app.route('/insert_task', methods=['POST'])
+def insert_task():
+    tasks = mongo.db.tasks
+    tasks.insert_one(request.form.to_dict())
+    return redirect(url_for('get_tasks'))
 
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP'),
             port=int(os.environ.get('PORT')),
             debug=True)
+
+# if __name__ == '__main__':
+#   app.run(host=os.environ.get('IP', '127.0.0.1'),
+#          port=int(os.environ.get('PORT', '8080')),
+#         debug=True)
