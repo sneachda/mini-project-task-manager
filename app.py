04237dcc4a86039b3e33dae1014d2933496db99e
sneachda <savela.aleks@gmail.com>
2020-05-09 15:59:21 +0100

    added base template and updated tasks template

--- app.py
+++ app.py
@@ -1,12 +1,19 @@
 import os
-from flask import Flask
+from flask import Flask, render_template, redirect, request, url_for
+from flask_pymongo import PyMongo
+from bson.objectid import ObjectId
 
 app = Flask(__name__)
+app.config["MONGO_DBNAME"] = 'task_manager'
+app.config["MONGO_URI"] = "mongodb://localhost:27017/task_manager"
+
+mongo = PyMongo(app)
 
 
 @app.route('/')
-def hello_world():
-    return 'Hello World!'
+@app.route('/get_tasks')
+def get_tasks():
+    return render_template("tasks.html", tasks=mongo.db.tasks.find())
 
 
 if __name__ == '__main__':
