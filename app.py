36d653a33aaafdc238e079e053202d4275863b94
sneachda <savela.aleks@gmail.com>
2020-05-10 18:42:16 +0100

    updated Mongo URI

--- app.py
+++ app.py
@@ -5,6 +5,7 @@ from bson.objectid import ObjectId
 
 app = Flask(__name__)
 
+
 app.config["MONGO_URI"] = 'mongodb+srv://root:r00tUser@myfirstcluster-owxyk.mongodb.net/task_manager?retryWrites=true&w=majority'
 
 mongo = PyMongo(app)
