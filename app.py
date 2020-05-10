c59f4ebe3e98aa867d929265a976a5bacbb74761
sneachda <savela.aleks@gmail.com>
2020-05-10 21:17:00 +0100

    updated MONGO URI

--- app.py
+++ app.py
@@ -3,10 +3,13 @@ from flask import Flask, render_template, redirect, request, url_for
 from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
 
+from os import path
+if path.exists("env.py"):
+    import env
+
 app = Flask(__name__)
-
-
-app.config["MONGO_URI"] = 'mongodb+srv://root:r00tUser@myfirstcluster-owxyk.mongodb.net/task_manager?retryWrites=true&w=majority'
+app.config["MONGO_DBNAME"] = 'task_manager'
+app.config["MONGO_URI"] = os.environ.get("MONGO_URI")
 
 mongo = PyMongo(app)
 
@@ -96,8 +99,8 @@ def add_category():
 
 
 if __name__ == '__main__':
-    app.run(host=os.environ.get('IP'),
-            port=int(os.environ.get('PORT')),
+    app.run(host=os.getenv("IP", "0.0.0.0"),
+            port=int(os.getenv("PORT", "5000")),
             debug=True)
 
 
