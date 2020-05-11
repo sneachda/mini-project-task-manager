617994259c5ecd203e597b4ede45ea6c20878abd
sneachda <savela.aleks@gmail.com>
2020-05-11 07:25:34 +0100

    SSL updated

--- app.py
+++ app.py
@@ -3,13 +3,14 @@ from flask import Flask, render_template, redirect, request, url_for
 from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
 
-from os import path
-if path.exists("env.py"):
-    import env
+#from os import path
+#if path.exists("env.py"):
+#    import env
 
 app = Flask(__name__)
 app.config["MONGO_DBNAME"] = 'task_manager'
-app.config["MONGO_URI"] = os.environ.get("MONGO_URI")
+#app.config["MONGO_URI"] = os.environ.get("MONGO_URI")
+app.config["MONGO_URI"] = 'mongodb+srv://root:r00tUser@myfirstcluster-owxyk.mongodb.net/task_manager?retryWrites=true&w=majority?'
 
 mongo = PyMongo(app)
 
